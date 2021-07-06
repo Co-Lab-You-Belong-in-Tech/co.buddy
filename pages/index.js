8f5697c15e66f45759aa2264059dc56b4c1e5b18
hkmjtech <ajmlhkm@gmail.com>
2021-07-06 14:43:31 +0800

    image issue not overlapping

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../public/resources/logo.png'
+import samkov from '../public/resources/samkov.png'
+import vlada from '../public/resources/vlada.png'
 
 export default function Home() {
   return ( 
@@ -21,19 +23,27 @@ export default function Home() {
         <meta name='theme-color' content='#317EFB' />
       </Head>
 
-      <div className="h-screen container mx-auto flex flex-col px-7 py-4 items-center justify-end">
-        <Image width="50" height="50" src={logo} alt="Co.Buddy logo" className="object-contain"></Image>
-        <div className="lg:flex-grow h-3/6 md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-center md:text-left md:mb-0 items-center text-center">
-          <h1 className="text-xl font-bold mb-10">
-            Welcome to Co.Buddy
-          </h1>
-          <p className="mb-32 leading-relaxed max-w-xs text-gray-500">Co.Buddy helps young professionals beat procastination and inactivity through peer-accountablity</p>
-          <div className="flex w-full justify-center mb-5">
-            <Link href="/enter">
-              <button className="text-white w-full text-center bg-copurple border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Get Started</button>
-            </Link>
+      <div className="h-screen relative container mx-auto flex flex-col px-7 py-4 items-center lg:justify-center justify-end">
+        <div className="absolute bottom-12 right-24 z-0">
+          <Image src={vlada} width="355" height="441"></Image>
+        </div>
+        <div className="absolute top-12 left-24 z-0">
+          <Image src={samkov} width="355" height="441"></Image>
+        </div>
+        <div className="absolute flex flex-col items-center lg:p-12 lg:shadow-xl rounded border-0 z-40">
+          <Image width="50" height="50" src={logo} alt="Co.Buddy logo" className="object-contain"></Image>
+          <div className="lg:flex-grow h-3/6 flex flex-col md:items-center md:text-left md:mb-0 items-center text-center">
+            <h1 className="text-xl font-bold mb-10">
+              Welcome to Co.Buddy
+            </h1>
+            <p className="mb-32 lg:mb-12 leading-relaxed max-w-xs text-gray-600 text-center">Co.Buddy helps young professionals beat procastination and inactivity through peer-accountablity</p>
+            <div className="flex w-full justify-center mb-5">
+              <Link href="/enter">
+                <button className="text-white w-full text-center bg-copurple border-0 py-2 px-6 focus:outline-none rounded text-lg">Get Started</button>
+              </Link>
+            </div>
+            <p className="mb-8 leading-relaxed text-sm text-gray-400">Already have an account? <Link href="/signin"><a className="text-copurple">Sign in</a></Link></p>
           </div>
-          <p className="mb-8 leading-relaxed text-sm text-gray-400">Already have an account? <Link href="/signin"><a className="text-copurple">Sign in</a></Link></p>
         </div>
       </div>
 
